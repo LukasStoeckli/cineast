a12635780b79637dbee06b1a86a76e170ef66337
Lukas Stöckli <lukas.stoeckli@stud.unibas.ch>
2019-04-02 04:05:44 +0200

    api: add IIIF-request object, more detailed request error

---
+++ src/org/vitrivr/cineast/api/rest/iiif/IIIFRequest.java
@@ -0,0 +1,80 @@
+package org.vitrivr.cineast.api.rest.iiif;
+
+import com.eclipsesource.json.JsonObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Lukas on 02.04.19.
+ */
+public class IIIFRequest {
+    // request info
+    private String institution;
+    private String endpoint;
+    private ArrayList<Content> content;
+    // error
+    private JsonObject requestError;
+    private JsonObject processError;
+    // getters
+    public String getInstitution() {return institution;}
+    public String getEndpoint() {return endpoint;}
+    public ArrayList<Content> getContent() {return content;}
+    public JsonObject getRequestError() {return requestError;}
+    // setters
+    public void setInstitution(String _institution) {institution = _institution;}
+    public void setEndpoint(String _endpoint) {endpoint = _endpoint;}
+    public void setRequestError(JsonObject _requestError) {requestError = _requestError;}
+    // add content
+    public void addContent(String _project, String _image, String _meta) {
+        content.add(new Content(_project, _image, _meta));
+    }
+    // constructor
+    public IIIFRequest() {
+        content = new ArrayList<>();
+    }
+
+
+
+
+
+    // check somewhere what data is already in db
+    // ditch invalid links? validate all urls in time for a response...
+
+
+    // get images and start exporter
+    public JsonObject process() {
+
+        // foo.resolve(this)
+        // bar.extract(whatever)
+
+
+        return null;
+    }
+
+
+
+
+
+
+
+
+
+}
+
+
+
+class Content {
+    private String project;
+    private String image;
+    private String meta;
+
+    public Content(String _project, String _image, String _meta) {
+        project = _project;
+        image = _image;
+        meta = _meta;
+    }
+
+    public String getProject() {return project;}
+    public String getImage() {return image;}
+    public String getMeta() {return meta;}
+}
--- src/org/vitrivr/cineast/api/rest/routes/IIIFRoute.java
+++ src/org/vitrivr/cineast/api/rest/routes/IIIFRoute.java
@@ -1,9 +1,12 @@
 package org.vitrivr.cineast.api.rest.routes;
 
+
+
 import com.eclipsesource.json.JsonArray;
 import com.eclipsesource.json.JsonObject;
 import com.eclipsesource.json.JsonValue;
 import org.vitrivr.cineast.api.rest.exceptions.MethodNotSupportedException;
+import org.vitrivr.cineast.api.rest.iiif.IIIFRequest;
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -18,13 +21,17 @@ public class IIIFRoute implements Route {
     System.out.println("client user-agent: " + request.userAgent());
     System.out.println("method: " + request.requestMethod());
     System.out.println("type: " + request.contentType());
+    System.out.println("protocol: " + request.protocol());
+    System.out.println("headers: " + request.headers());
     System.out.println("body: " + request.body());
 
-    response.type("application/json");
+
     switch(request.requestMethod()) {
       case "GET":
+        response.type("text/html");
         return getHelp();
       case "POST":
+        response.type("application/json");
         return handleRequest(request);
       default:
         throw new MethodNotSupportedException(request);
@@ -35,88 +42,21 @@ public class IIIFRoute implements Route {
 
   private String handleRequest(Request request) {
     JsonObject json = JsonObject.readFrom(request.body());
+    IIIFRequest requestObject = validate(json);
     JsonObject response = new JsonObject();
+    if (requestObject.getRequestError() != null) {
+      response = requestObject.getRequestError();
+    } else {
+      // start process here
 
-    // validate & sanitize input
-    int errors = 0;
-    String errorMsg = null;
-    String institution = "";
-    String iiifEndpoint = "";
-    JsonArray content = new JsonArray();
-
-    // institution
-    try {
-      institution = json.get("institution").asString();
-      if (institution.equals("")) { throw new NullPointerException(); }
-    } catch (NullPointerException e) {
-      errorMsg = "missing value for institution";
-      errors += 1;
-    }
-    // iiifEndpoint
-    try {
-      iiifEndpoint = json.get("iiifEndpoint").asString();
-      if (iiifEndpoint.equals("")) { throw new NullPointerException(); }
-    } catch (NullPointerException e) {
-      errorMsg = "missing value for iiifEndpoint";
-      errors += 1;
-    } // content
-    try {
-      content = json.get("content").asArray();
-      if (content.size() == 0) { throw new UnsupportedOperationException(); }
-      // also check if content elements are correct!!!
-      for (int i = 0; i < content.size(); i++) {
-        // TO DO
-      }
-    } catch (NullPointerException e) {
-      errorMsg = "missing value for content";
-      errors += 1;
-    } catch (UnsupportedOperationException e) {
-      errorMsg = "content must be nonempty array";
-      errors += 1;
-    }
-
-    // invalid request
-    if (errors != 0) {
-      response.add("status","400");
-      response.add("errorMsg", errorMsg);
-      return response.toString();
-    }
-
-
-
-    // ---------------------------------
-    // VALID NONEMPTY PARAMS
-    //        |
-    //        |
-    //        V
-
-
-
-
-
-
-    // process content
-    // figure out how response looks like when not everything is checked yes
-    // extraction in new thread, maybe tell result somehow
-
-
-    System.out.println("contentSize: " + content.size());
-    for (int i = 0; i < content.size(); i++) {
-      System.out.println(i + "th elem: " + content.get(i));
+      response.add("status","200");
+      response.add("institution", requestObject.getInstitution());
+      response.add("numberOfResources", requestObject.getContent().size());
     }
 
 
 
 
-
-
-
-
-
-
-    response.add("status","200");
-    response.add("institution", institution);
-    response.add("numberOfResources", content.size());
 
 
     return response.toString();
@@ -134,6 +74,99 @@ public class IIIFRoute implements Route {
     // add definition of fields
     // add example request
     // add example responses
-    return help.toString();
+    return "<h1>IIIF Endpoint</h1><p>Add API specification here</p>";//help.toString();
   }
+
+
+
+
+  public IIIFRequest validate(JsonObject json) {
+    String institution = "";
+    String iiifEndpoint = "";
+    JsonArray content = new JsonArray();
+
+    int errors = 0;
+    JsonArray errorMsg = new JsonArray();
+
+    IIIFRequest requestObject = new IIIFRequest();
+
+    // institution
+    try {
+      institution = json.get("institution").asString();
+      if (institution.equals("")) {
+        errorMsg.add("empty value for institution");
+        errors += 1;
+      }
+      requestObject.setInstitution(institution);
+    } catch (NullPointerException e) {
+      errorMsg.add("missing value for institution");
+      errors += 1;
+    }
+
+    // iiifEndpoint
+    try {
+      iiifEndpoint = json.get("iiifEndpoint").asString();
+      if (iiifEndpoint.equals("")) {
+        errorMsg.add("empty value for iiifEndpoint");
+        errors += 1;
+      }
+      requestObject.setEndpoint(iiifEndpoint);
+    } catch (NullPointerException e) {
+      errorMsg.add("missing value for iiifEndpoint");
+      errors += 1;
+    }
+
+    // content
+    try {
+      content = json.get("content").asArray();
+      if (content.size() == 0) {
+        errorMsg.add("content must be nonempty array");
+        errors += 1;
+      }
+      for (int i = 0; i < content.size(); i++) {
+        System.out.println(i + "th elem: " + content.get(i));
+
+        String project;
+        String image;
+        String meta;
+
+        /*
+        try {
+
+        } catch () {
+          errorMsg.add("");
+          errors += 1;
+          continue;
+        }
+        */
+
+
+        //requestObject.addContent(project, image, meta);
+      }
+    } catch (NullPointerException e) {
+      errorMsg.add("missing value for content");
+      errors += 1;
+    }
+
+
+
+
+
+    // invalid request
+    if (errors != 0) {
+      JsonObject requestError = new JsonObject();
+      requestError.add("status","400");
+      requestError.add("errors", errors);
+      requestError.add("errorMessage", errorMsg);
+      requestObject.setRequestError(requestError);
+    }
+
+
+
+
+
+    return requestObject;
+  }
+
+
 }
